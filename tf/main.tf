9096eab49031592df4e7928b0f2dcf222bbdb34a
Travis Douglas <tedward27@gmail.com>
2025-05-07 00:19:47 -0500

    add lambda function

--- tf/main.tf
+++ tf/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.67"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.11.0"
 }
 
 provider "aws" {
@@ -43,5 +43,25 @@ resource "aws_s3_bucket" "s3_lambda" {
     product = "sand"
     env = "dev"
     name = "lambda-s3"
+  }
+}
+
+resource "aws_lambda_function" "lambda_openpowerlifting_tf" {
+  function_name = "ted-sand-dev-use2-ingest-openpowerlifting-tf"
+  role = "arn:aws:iam::820242901733:role/service-role/LambdaIngestPowerliftingRole"
+  filename = "../python/lambda_ingest_openpowerlifting.zip"
+  handler = "lambda_ingest_openpowerlifting.lambda_handler"
+  runtime = "python3.9"
+  layers = [
+    "arn:aws:lambda:us-east-2:336392948345:layer:AWSSDKPandas-Python313:1",
+  ]
+  memory_size = 3008
+  publish = false
+  timeout = 60
+  tags = {
+    org = "ted"
+    product = "sand"
+    env = "dev"
+    name = "openpowerlifting-lambda-tf"
   }
 }
