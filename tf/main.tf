c2273969e48737b020b6161e850d2bca88bb23b9
Travis Douglas <tedward27@gmail.com>
2025-05-07 01:56:59 -0500

    add athena workgroup

--- tf/main.tf
+++ tf/main.tf
@@ -141,3 +141,24 @@ resource "aws_lambda_permission" "allow_eventbridge_openpowerlifting" {
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.daily_rule.arn
 }
+
+variable "aws_account_id" {
+  type    = string
+  default = "820242901733"
+}
+
+resource "aws_athena_workgroup" "primary" {
+  name = "primary"
+  configuration {
+    result_configuration {
+      expected_bucket_owner = var.aws_account_id
+      output_location = "s3://${aws_s3_bucket.s3_athena.bucket}/primary/"
+      acl_configuration {
+        s3_acl_option = "BUCKET_OWNER_FULL_CONTROL"
+      }
+      encryption_configuration {
+        encryption_option = "SSE_S3"
+      }
+    }
+  }
+}
