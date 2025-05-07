407e42f4ab2d07e1ee3eecaa44d67c535ab58692
Travis Douglas <tedward27@gmail.com>
2025-05-06 21:43:33 -0500

    fix lambda

--- python/lambda_ingest_openpowerlifting.py
+++ python/lambda_ingest_openpowerlifting.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 import logging
 from zipfile import ZipFile
 from io import BytesIO
@@ -6,13 +7,7 @@ import boto3
 import requests
 
 
-logger = logging.getLogger()
-logger.setLevel("INFO")
-
-s3 = boto3.client('s3')
-
-
-def lambda_handler(event, context):
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     """
     Main Lambda handler function
     Parameters:
@@ -23,14 +18,20 @@ def lambda_handler(event, context):
     """
 
     try:
+        logger = logging.getLogger()
+        logger.setLevel("INFO")
+        s3 = boto3.client("s3")
+
         url = "https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip"
         bucket = "ted-sand-dev-s3-use2-data"
 
+        logger.info("Requesting response from HTTP endpoint")
+        logger.info(f"URL: {url}")
         response = requests.get(url)
         response.raise_for_status()
 
+        logger.info("Opening ZIP file")
         with ZipFile(BytesIO(response.content)) as z:
-
             csv_files = [name for name in z.namelist() if name.endswith(".csv")]
 
             if not csv_files:
@@ -40,14 +41,15 @@ def lambda_handler(event, context):
             csv_fn = csv_path.split("/")[-1]
             key = f"openpowerlifting/{csv_fn}"
 
+            logger.info("Uploading CSV file to S3")
+            logger.info(f"Bucket: {bucket}")
+            logger.info(f"Key: {key}")
             with z.open(csv_path, "r") as csv_file:
                 s3.upload_fileobj(csv_file, bucket, key)
-        
-        return {
-            "statusCode": 200,
-            "message": f"File loaded successfully from OPL to S3, {url}, {bucket}, {key}"
-        }
+
+        logger.info("File ingested successfully")
+        return {"statusCode": 200, "message": f"File ingested successfully."}
 
     except Exception as e:
         logger.error(f"Error loading file: {str(e)}")
-        raise
+        return {"statusCode": 500, "message": f"File ingestion failed."}
