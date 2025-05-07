3a7903e2493eeb141cb3e254dc08220bbd776f6e
Travis Douglas <tedward27@gmail.com>
2025-05-07 02:55:07 -0500

    add input validation

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,14 +1,28 @@
 variable "lambda_openpowerlifting_filename" {
   type    = string
   default = "../python/lambda_ingest_openpowerlifting.zip"
+  validation {
+    condition     = can(regex("\\.zip$", var.lambda_openpowerlifting_filename))
+    error_message = "Filename must be a .zip file"
+  }
+
 }
 
 variable "lambda_openpowerlifting_handler" {
   type    = string
   default = "lambda_ingest_openpowerlifting.lambda_handler"
+  validation {
+    condition     = can(regex("\\.lambda_handler$", var.lambda_openpowerlifting_handler))
+    error_message = "Function must be a lambda handler"
+  }
 }
 
 variable "aws_account_id" {
   type    = string
   default = "820242901733"
+  validation {
+    condition     = can(regex("^[0-9]{12}$", var.aws_account_id))
+    error_message = "AWS Account ID must be a 12-digit number"
+  }
+
 }
