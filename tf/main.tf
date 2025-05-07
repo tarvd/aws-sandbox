51ae2f769558ab06545147c6159818b078a2dbb1
Travis Douglas <tedward27@gmail.com>
2025-05-06 23:33:16 -0500

    added S3 buckets to TF

--- tf/main.tf
+++ tf/main.tf
@@ -12,3 +12,36 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 }
+
+resource "aws_s3_bucket" "s3_athena" {
+  bucket = "ted-sand-dev-s3-use2-athena"
+
+  tags = {
+    org = "ted"
+    product = "sand"
+    env = "dev"
+    name = "athena-s3"
+  }
+}
+
+resource "aws_s3_bucket" "s3_data" {
+  bucket = "ted-sand-dev-s3-use2-data"
+
+  tags = {
+    org = "ted"
+    product = "sand"
+    env = "dev"
+    name = "data-s3"
+  }
+}
+
+resource "aws_s3_bucket" "s3_lambda" {
+  bucket = "ted-sand-dev-s3-use2-lambda"
+
+  tags = {
+    org = "ted"
+    product = "sand"
+    env = "dev"
+    name = "lambda-s3"
+  }
+}
