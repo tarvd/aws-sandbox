c7a5e6d4ae574bdbda29dee2b3b57fc9d2d2f0bc
Travis Douglas <tedward27@gmail.com>
2025-08-11 23:00:27 -0500

    ruff

--- python/lambda_ingest_openpowerlifting.py
+++ python/lambda_ingest_openpowerlifting.py
@@ -7,6 +7,10 @@ import boto3
 import requests
 
 
+URL = "https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip"
+BUCKET = "ted-sand-dev-s3-use2-data"
+
+
 def lambda_handler(event: dict[str, Any], context: Any) -> dict[str, Any]:
     """
     Ingests the latest Open Powerlifting CSV file from openpowerlifting.org and uploads it to S3.
@@ -17,46 +21,40 @@ def lambda_handler(event: dict[str, Any], context: Any) -> dict[str, Any]:
         Dict containing status message
     """
 
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        filename="lambda_ingest_openpowerlifting.log",
+        encoding="utf-8",
+        level=logging.INFO,
+    )
+    logger.info(
+        f"Lambda function for Openpowerlifting data ingestion started. URL: {URL}, Bucket: {BUCKET}"
+    )
+
+    s3 = boto3.client("s3")
+
     try:
-        logger = logging.getLogger(__name__)
-        logging.basicConfig(
-            filename="lambda_ingest_openpowerlifting.log",
-            encoding="utf-8",
-            level=logging.INFO,
-        )
-        logger.info("Starting Lambda function")
-        s3 = boto3.client("s3")
-
-        url = "https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip"
-        bucket = "ted-sand-dev-s3-use2-data"
-
-        logger.info("Requesting response from HTTP endpoint")
-        logger.info(f"URL: {url}")
-        response = requests.get(url)
+        response = requests.get(URL)
         response.raise_for_status()
 
-        logger.info("Opening ZIP file")
         with ZipFile(BytesIO(response.content)) as z:
             csv_files = [name for name in z.namelist() if name.endswith(".csv")]
-
             if not csv_files:
                 raise ValueError("No CSV files found in the ZIP archive.")
 
             csv_path = csv_files[0]
             csv_fn = csv_path.split("/")[-1]
             key = f"openpowerlifting/{csv_fn}"
-
-            logger.info("Uploading CSV file to S3")
-            logger.info(f"Bucket: {bucket}")
-            logger.info(f"Key: {key}")
             with z.open(csv_path, "r") as csv_file:
-                s3.upload_fileobj(csv_file, bucket, key)
+                s3.upload_fileobj(csv_file, BUCKET, key)
 
-        logger.info("File ingested successfully")
-        logger.info("Ending Lambda function")
+        logger.info(
+            "Lambda function for Openpowerlifting data ingestion ended: Success!"
+        )
         return {"statusCode": 200, "message": "File ingested successfully."}
 
     except Exception as e:
-        logger.error(f"Error loading file: {str(e)}")
-        logger.info("Ending Lambda function")
+        logger.error(
+            f"Lambda function for Openpowerlifting data ingestion ended: Failure. {str(e)}"
+        )
         return {"statusCode": 500, "message": "File ingestion failed."}
