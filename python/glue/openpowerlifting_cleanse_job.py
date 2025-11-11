16f5bd352818fa96aa3dcdff1bb0007b9f2252f1
Travis Douglas <tedward27@gmail.com>
2025-11-11 10:46:38 -0600

    clean up cleanse job script

--- python/glue/openpowerlifting_cleanse_job.py
+++ python/glue/openpowerlifting_cleanse_job.py
@@ -1,33 +1,32 @@
-
-# TODO: Add output logging
-# TODO: Clean up constants
-
-
 import sys
+import logging
+import boto3
+from datetime import datetime, timezone
+
+
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-from awsglue.job import Job
-
-
-import logging
-import boto3
-from datetime import datetime, timezone
 from pyspark.sql.functions import lit, concat_ws, current_timestamp, coalesce, sha2
 
 
-MSG_FORMAT = '%(asctime)s %(levelname)s %(name)s: %(message)s'
-DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+# Logging
+MSG_FORMAT = "%(asctime)s %(levelname)s %(name)s: %(message)s"
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+logging.basicConfig(format=MSG_FORMAT, datefmt=DATETIME_FORMAT)
+logger = logging.getLogger("glue-log")
+logger.setLevel(logging.INFO)
 
-# Load Constants and Config
-source_db = "raw"
-source_table = "openpowerlifting"
-source_system = "Openpowerlifting.org"
-target_db = "cleansed"
-target_table = "openpowerlifting"
-context = "RawToIcebergCleanseJob"
-processed_data_cols = [
+
+# Constants and Config
+SOURCE_DB = "raw"
+SOURCE_TABLE = "openpowerlifting"
+SOURCE_SYSTEM = "Openpowerlifting.org"
+TARGET_DB = "cleansed"
+TARGET_TABLE = "openpowerlifting"
+CONTEXT = "RawToIcebergCleanseJob"
+PROCESSED_DATA_COLS = [
     "file_key",
     "job_name",
     "context",
@@ -35,7 +34,7 @@ processed_data_cols = [
     "job_id",
     "job_run_id",
 ]
-rename_cols_map = {
+RENAME_COLS_MAP = {
     "Name": "name",
     "Sex": "sex",
     "Event": "event",
@@ -82,33 +81,23 @@ rename_cols_map = {
 
 
 def main():
+    try:
+        logger.info("Beginning job, loading arguments.")
 
-    try: 
         sc = SparkContext.getOrCreate()
         glueContext = GlueContext(sc)
         spark = glueContext.spark_session
-
-        
-        logging.basicConfig(format=MSG_FORMAT, datefmt=DATETIME_FORMAT)
-        logger = logging.getLogger('glue-log')
-        logger.setLevel(logging.INFO)
-
+        glue = boto3.client("glue")
+        s3 = boto3.client("s3")
 
         args = getResolvedOptions(sys.argv, ["JOB_NAME"])
         job_name = args["JOB_NAME"]
         job_id = args["JOB_ID"]
         job_run_id = args["JOB_RUN_ID"]
 
-        
-        logger.info(f"Beginning job: {job_name}")
+        logger.info(f"Reading list of files for {SOURCE_DB}.{SOURCE_TABLE}")
 
-        
-        # Get list of table S3 files
-        glue = boto3.client("glue")
-        s3 = boto3.client("s3")
-
-        logger.info(f"Reading list of files for {source_db}.{source_table}")
-        source_location = glue.get_table(DatabaseName=source_db, Name=source_table)[
+        source_location = glue.get_table(DatabaseName=SOURCE_DB, Name=SOURCE_TABLE)[
             "Table"
         ]["StorageDescriptor"]["Location"]
         split = source_location.split("/")
@@ -125,7 +114,7 @@ def main():
             source_objects.extend([obj["Key"] for obj in response["Contents"]])
 
         skip_files_df = spark.sql(
-            f"SELECT file_key FROM glue_catalog.metadata.processed_data_log WHERE job_name='{job_name}' AND context='{context}'"
+            f"SELECT file_key FROM glue_catalog.metadata.processed_data_log WHERE job_name='{job_name}' AND context='{CONTEXT}'"
         )
         skip_files_list = (
             skip_files_df.select("file_key").rdd.flatMap(lambda x: x).collect()
@@ -137,20 +126,23 @@ def main():
                 if f"s3://{source_bucket}/{path}" not in skip_files_list
             ]
         )
+
         logger.info(f"Files to process: {source_file_list}")
 
-        # Loop through files to read
+        # Process each file
         for filename in source_file_list:
+            logger.info(f"Processing file: {filename}")
 
-            logger.info(f"Reading file: {filename}")
-
-            # Data to insert into Processed Data Log table (replicating functionality of Glue Job Bookmark)
+            # Keep track of processed files
             processing_timestamp = datetime.now(timezone.utc)
             processed_data_row = [
-                (filename, job_name, context, processing_timestamp, job_id, job_run_id)
+                (filename, job_name, CONTEXT, processing_timestamp, job_id, job_run_id)
             ]
+            processed_data_df = spark.createDataFrame(
+                processed_data_row, PROCESSED_DATA_COLS
+            )
 
-            # Read source data
+            logger.info("Reading source file into Spark DF")
             df = glueContext.create_data_frame.from_options(
                 connection_type="s3",
                 connection_options={"paths": [filename]},
@@ -158,71 +150,60 @@ def main():
                 format_options={"withHeader": True},
             )
             source_num_rows = df.count()
-            logger.info(f"Number of rows loaded to Spark DF: {source_num_rows}")
+            logger.info(f"Number of rows in source {filename}: {source_num_rows}")
 
-            processed_data_df = spark.createDataFrame(
-                processed_data_row, processed_data_cols
-            )
-
-            # Get already-existing row hashes from target table to filter source data
-            logger.info(f"Reading target table {target_db}.{target_table} row IDs")
-            target_rows_df = spark.sql(
-                f"SELECT row_hash FROM glue_catalog.{target_db}.{target_table}"
-            )
-
-            # Hash the source data rows, deduplicate on hash, and remove rows that already exist in the target
-            logger.info("Dropping duplicates and already-existing rows")
+            logger.info("Dropping duplicates and rows existing in target")
             df = df.withColumn(
                 "row_hash",
                 sha2(
                     concat_ws(
-                        ":", *[coalesce(col, lit("")).alias(col) for col in df.schema.names]
+                        ":",
+                        *[coalesce(col, lit("")).alias(col) for col in df.schema.names],
                     ),
                     256,
                 ),
             )
             df = df.dropDuplicates(["row_hash"])
+            target_rows_df = spark.sql(
+                f"SELECT row_hash FROM glue_catalog.{TARGET_DB}.{TARGET_TABLE}"
+            )
             df = (
                 df.join(target_rows_df, ["row_hash"], "leftouter")
                 .where(target_rows_df["row_hash"].isNull())
                 .drop(target_rows_df["row_hash"])
             )
             filtered_num_rows = df.count()
-            logger.info(f"Number of rows that will be loaded to target: {filtered_num_rows}")
+            logger.info(
+                f"Number of rows that will be loaded to target: {filtered_num_rows}"
+            )
 
-            # Add audit columns
             logger.info("Adding audit columns")
-            df = df.withColumn("source_system", lit(source_system))
-            df = df.withColumn("source_table", lit(f"{source_db}.{source_table}"))
+            df = df.withColumn("source_system", lit(SOURCE_SYSTEM))
+            df = df.withColumn("source_table", lit(f"{SOURCE_DB}.{SOURCE_TABLE}"))
             df = df.withColumn("inserted_at", current_timestamp())
             df = df.withColumn("job_name", lit(job_name))
             df = df.withColumn("job_id", lit(job_id))
             df = df.withColumn("job_run_id", lit(job_run_id))
 
-            # Rename camel case to snake case
             logger.info("Renaming columns from camel to snake case")
-            df = df.withColumnsRenamed(rename_cols_map)
+            df = df.withColumnsRenamed(RENAME_COLS_MAP)
 
-            # Write data to target
-            logger.info(f"Writing data to target {target_db}.{target_table}")
-            df.writeTo(f"glue_catalog.{target_db}.{target_table}").tableProperty(
+            logger.info(f"Writing data to target {TARGET_DB}.{TARGET_TABLE}")
+            df.writeTo(f"glue_catalog.{TARGET_DB}.{TARGET_TABLE}").tableProperty(
                 "format-version", "2"
             ).append()
 
-            # Write row to Processed Data Log
-            logger.info(f"Adding row to Processed Data Log for {filename}")
+            logger.info(f"Adding row to metadata.processed_data_log for {filename}")
             processed_data_df.writeTo(
                 "glue_catalog.metadata.processed_data_log"
             ).tableProperty("format-version", "2").append()
 
-
         logger.info(f"Job {job_name} completed successfully")
 
-
     except Exception as e:
-        logger.error(f"Error: {e}")
-        sys.exit(1)
+        logger.error(f"Error: {e.message}")
+        raise e
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
