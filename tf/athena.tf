8012dc2f5cf808038a07d7ab75325a5a2323f9df
Travis Douglas <tedward27@gmail.com>
2025-11-11 11:40:08 -0600

    add variables to tf

--- tf/athena.tf
+++ tf/athena.tf
@@ -1,14 +1,20 @@
 resource "aws_athena_workgroup" "primary" {
-  name = "primary"
+  name = var.athena_workgroup_primary.name
+
   configuration {
+    enforce_workgroup_configuration    = var.athena_workgroup_primary.enforce_workgroup_configuration
+    publish_cloudwatch_metrics_enabled = var.athena_workgroup_primary.publish_cloudwatch_metrics_enabled
+
     result_configuration {
       expected_bucket_owner = var.aws_account_id
       output_location       = "s3://${aws_s3_bucket.athena_results.bucket}/primary/"
+
+      encryption_configuration {
+        encryption_option = var.athena_workgroup_primary.encryption_option
+      }
+
       acl_configuration {
         s3_acl_option = "BUCKET_OWNER_FULL_CONTROL"
-      }
-      encryption_configuration {
-        encryption_option = "SSE_S3"
       }
     }
   }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,3 +1,32 @@
+
+# Top level vars
+
+variable "aws_account_id" {
+  type    = string
+  default = "820242901733"
+  validation {
+    condition     = can(regex("^[0-9]{12}$", var.aws_account_id))
+    error_message = "AWS Account ID must be a 12-digit number"
+  }
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+
+# Athena vars
+
+variable "athena_workgroup_primary" {
+  type = object({
+    name = string
+    enforce_workgroup_configuration = bool
+    publish_cloudwatch_metrics_enabled = bool
+    encryption_option = string
+  })
+}
+
 variable "lambda_openpowerlifting_filename" {
   type    = string
   default = "../python/lambda/lambda_ingest_openpowerlifting.zip"
@@ -15,14 +44,4 @@ variable "lambda_openpowerlifting_handler" {
     condition     = can(regex("\\.lambda_handler$", var.lambda_openpowerlifting_handler))
     error_message = "Function must be a lambda handler"
   }
-}
-
-variable "aws_account_id" {
-  type    = string
-  default = "820242901733"
-  validation {
-    condition     = can(regex("^[0-9]{12}$", var.aws_account_id))
-    error_message = "AWS Account ID must be a 12-digit number"
-  }
-
 }
