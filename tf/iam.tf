76b4a1bc22d4b6a3d79212b6235fbda4197bc84e
Travis Douglas <tedward27@gmail.com>
2025-11-11 22:50:31 -0600

    iam refactoring

--- tf/iam.tf
+++ tf/iam.tf
@@ -27,7 +27,7 @@ resource "aws_iam_policy" "glue_job_policy" {
   name        = var.iam_policy_glue_job.name
   description = var.iam_policy_glue_job.description
   policy = templatefile(
-    "./policies/glue_job_policy.json.tpl",
+    "./policies/glue_job_policy.json",
     {
       python_s3_bucket = "${aws_s3_bucket.python.bucket}"
       raw_data_s3_bucket = "${aws_s3_bucket.raw_data.bucket}"
@@ -43,105 +43,13 @@ resource "aws_iam_role_policy_attachment" "glue_job_attach" {
 resource "aws_iam_policy" "glue_notebook_policy" {
   name        = var.iam_policy_glue_notebook.name
   description = var.iam_policy_glue_notebook.description
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      # --- Glue permissions (catalog, jobs, notebooks) ---
-      {
-        Effect = "Allow"
-        Action = [
-          "glue:GetDatabase",
-          "glue:GetDatabases",
-          "glue:GetTable",
-          "glue:GetTables",
-          "glue:CreateTable",
-          "glue:UpdateTable",
-          "glue:DeleteTable",
-          "glue:GetPartition",
-          "glue:GetPartitions",
-          "glue:CreatePartition",
-          "glue:UpdatePartition",
-          "glue:DeletePartition",
-          "glue:GetConnection",
-          "glue:GetJob",
-          "glue:StartJobRun",
-          "glue:GetJobRun",
-          "glue:GetJobRuns",
-          "glue:BatchStopJobRun",
-          "glue:CreateSession",
-          "glue:GetSession",
-          "glue:GetSessions",
-          "glue:StopSession",
-          "glue:TagResource",
-          "glue:RunStatement",
-          "glue:GetStatement"
-        ]
-        Resource = "*"
-      },
-
-      # --- S3 access for read/write data ---
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:ListBucket"
-        ]
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.raw_data.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.iceberg.bucket}"
-        ]
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ]
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.raw_data.bucket}/*",
-          "arn:aws:s3:::${aws_s3_bucket.iceberg.bucket}/*"
-        ]
-      },
-
-      # --- CloudWatch Logs for notebook output ---
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "arn:aws:logs:*:*:*"
-      },
-
-      # --- EC2 networking for Glue job runtime ---
-      {
-        Effect = "Allow"
-        Action = [
-          "ec2:CreateNetworkInterface",
-          "ec2:DeleteNetworkInterface",
-          "ec2:DescribeNetworkInterfaces",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeVpcs"
-        ]
-        Resource = "*"
-      },
-
-      # --- IAM PassRole so Glue can use this role ---
-      {
-        Effect = "Allow"
-        Action = "iam:PassRole"
-        Resource = "*"
-        Condition = {
-          StringEquals = {
-            "iam:PassedToService" = "glue.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
+  policy = templatefile(
+    "./policies/glue_notebook_policy.json",
+    {
+      python_s3_bucket = "${aws_s3_bucket.python.bucket}"
+      raw_data_s3_bucket = "${aws_s3_bucket.raw_data.bucket}"
+      iceberg_s3_bucket = "${aws_s3_bucket.iceberg.bucket}"
+    })
 }
 
 resource "aws_iam_role_policy_attachment" "glue_notebook_attach" {
@@ -160,19 +68,7 @@ data "aws_iam_policy" "AWSLambdaBasicExecutionRole" {
 resource "aws_iam_role" "lambda_role" {
   name        = var.iam_role_lambda.name
   description = var.iam_role_lambda.description
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
+  assume_role_policy = "${file("./policies/lambda_assume_role_policy.json")}"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda-role-s3-policy-attach" {
@@ -188,44 +84,13 @@ resource "aws_iam_role_policy_attachment" "lambda-role-lambda-policy-attach" {
 resource "aws_iam_role" "eb_start_workflow" {
   name = var.iam_role_eventbridge_start_workflow.name 
   description = var.iam_role_eventbridge_start_workflow.description
-  path = "/service-role/"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Sid       = "TrustEventBridgeService"
-      Condition = {
-        StringEquals = {
-          "aws:SourceAccount" = var.aws_account_id
-          "aws:SourceArn"     = aws_cloudwatch_event_rule.openpowerlifting.arn
-        }
-      }
-      Effect    = "Allow"
-      Principal = {
-        Service = "events.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
+  assume_role_policy = "${file("./policies/eventbridge_assume_role_policy.json")}"
 }
 
 resource "aws_iam_policy" "eb_start_workflow" {
   name        = var.iam_policy_eventbridge_start_workflow.name 
   description = var.iam_policy_eventbridge_start_workflow.description
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "ActionsForResource",
-            "Effect": "Allow",
-            "Action": [
-                "glue:NotifyEvent"
-            ],
-            "Resource": [
-                "arn:aws:glue:us-east-2:820242901733:workflow/dev-use2-tedsand-openpowerlifting-wf"
-            ]
-        }
-    ]
-  })
+  policy      = "${file("./policies/eventbridge_glue_policy.json")}"
 }
 
 resource "aws_iam_role_policy_attachment" "eb_start_workflow" {
